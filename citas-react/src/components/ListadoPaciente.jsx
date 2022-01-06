be4fb38203c4f05f59f92a9b6ba08b4e83a43b56
Jose Rincon <rincon.jmg@gmail.com>
2022-01-05 21:53:16 -0500

    iniciando proyecto

---
+++ citas-react/src/components/ListadoPaciente.jsx
@@ -0,0 +1,12 @@
+const ListadoPaciente = () => {
+    return (
+        <div className="w-1/2 lg:w-3/5">
+            <h2>
+                Listado Pacientes
+            </h2>
+        </div>
+    )
+}
+
+export default ListadoPaciente
+
