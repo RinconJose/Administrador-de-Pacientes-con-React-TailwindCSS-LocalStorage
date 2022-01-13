22815d93ddba44f68c38a807cf75d822444f7f15
Jose Rincon <rincon.jmg@gmail.com>
2022-01-12 22:14:32 -0500

    agregando botones de editar y eliminar la información de los pacientes

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -29,6 +29,18 @@ const Paciente = ({paciente}) => {
                 <span className="font-normal normal-case">{sintomas}</span>
             </p>
 
+            <div className="flex flex-wrap justify-between mt-10 gap-5">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                > Editar </button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                > Eliminar </button>
+
+            </div>
+
         </div>
     )
 }
