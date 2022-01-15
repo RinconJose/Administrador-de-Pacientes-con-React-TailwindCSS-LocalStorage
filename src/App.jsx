c04a37e2886990ce2de698ef47014d7ac1064054
Jose Rincon <rincon.jmg@gmail.com>
2022-01-15 12:55:57 -0500

    llenando los campos del formulario cuando le das click al botón de editar del paciente

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Formulario
           pacientes = {pacientes}
           setPacientes = {setPacientes}
+          paciente = {paciente}
         />
         <ListadoPaciente 
           pacientes={pacientes}
